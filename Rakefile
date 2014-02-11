9f51845bc4eca3c07f45b6dda821ddd082dc80e5
tom <tommaso@morsedigital.com>
2014-02-11 13:26:34 +0000

    added minitest

--- Rakefile
+++ Rakefile
@@ -4,6 +4,9 @@ require 'rbconfig'
 
 OS = RbConfig::CONFIG['host_os']
 
+require 'rake/testtask'
+
 Rake::TestTask.new do |t|
   t.libs << 'test'
+  t.pattern = "test/*_test.rb"
 end
---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'clickatell_gw'
+
+require 'minitest/unit'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# http://blakewilliams.me/blog/6-developing-gems-with-tdd-and-minitest-pt-1
+
